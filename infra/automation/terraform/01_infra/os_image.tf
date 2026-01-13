65a2171d8b7287688b9c416f8fa5448c4364012d
Romaric DEMBELE <romadembele@protonmail.com>
2026-01-13 07:54:04 +0000

    chore(terrform): reference project variable

--- infra/automation/terraform/01_infra/os_image.tf
+++ infra/automation/terraform/01_infra/os_image.tf
@@ -1,10 +1,10 @@
 data "google_compute_image" "ubuntu" {
   family  = "ubuntu-2404-lts-amd64"
-  project = "ubuntu-os-cloud"
+  project = var.project
 }
 
 resource "google_compute_disk" "persistent" {
-  name  = "vm-disk"
+  name  = "vm-disk-prod-97532680"
   image = data.google_compute_image.ubuntu.self_link
   size  = var.disk_size
   type  = "pd-standard"
