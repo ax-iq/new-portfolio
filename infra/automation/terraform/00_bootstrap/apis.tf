c90d5a0ea9ae366d6c9b76233e83b470239860f9
Romaric DEMBELE <romadembele@protonmail.com>
2026-01-13 19:52:06 +0000

    chore(terraform): format

--- infra/automation/terraform/00_bootstrap/apis.tf
+++ infra/automation/terraform/00_bootstrap/apis.tf
@@ -1,11 +1,11 @@
 resource "google_project_service" "compute_api" {
-  project = var.project
-  service = "compute.googleapis.com"
+  project            = var.project
+  service            = "compute.googleapis.com"
   disable_on_destroy = false
 }
 
 resource "google_project_service" "storage_api" {
-  project = var.project
-  service = "storage.googleapis.com"
+  project            = var.project
+  service            = "storage.googleapis.com"
   disable_on_destroy = false
 }
