e4c08c863c4b26d1d4a391528654b7809da77837
Romaric DEMBELE <romadembele@protonmail.com>
2026-01-14 01:28:28 +0000

    refactor(workflows): refactoring

--- infra/automation/terraform/00_bootstrap_copy_plus_backend/backend.tf
+++
@@ -1,6 +0,0 @@
-# Configure a remote backend like S3 to persist state
-terraform {
-  backend "gcs" {
-    prefix = "infra/state"
-  }
-}
--- infra/automation/terraform/01_infra/buckets.tf
+++ infra/automation/terraform/01_infra/buckets.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.8.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-}
-
 resource "google_storage_bucket" "tf_backend" {
   name                        = var.bucket_name
   location                    = var.region
