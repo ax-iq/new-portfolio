a9156b949f79948f44827009293353389318f454
Romaric DEMBELE <romadembele@protonmail.com>
2026-01-15 19:48:15 +0000

    chore(terraform): create new vm, firewalls

--- infra/automation/terraform/01_infra/main.tf
+++ infra/automation/terraform/01_infra/main.tf
@@ -1,37 +1,37 @@
-# resource "google_compute_instance" "vm_instance" {
-#   name         = "ubuntu-vm-prod-97532680"
-#   machine_type = "e2-micro"
-#   zone         = var.zone
+resource "google_compute_instance" "vm_instance" {
+  name         = "ubuntu-vm-prod-98532680"
+  machine_type = "e2-micro"
+  zone         = var.zone
 
-#   tags = ["allow-ssh", "http-server", "https-server"]
+  tags = ["allow-ssh", "http-server", "https-server"]
 
-#   metadata = {
-#     "user-data" = templatefile("templates/user-data.yaml.tftpl", {
-#       ssh_user       = var.ssh_user,
-#       ssh_public_key = var.ssh_public_key
-#     })
-#   }
+  metadata = {
+    "user-data" = templatefile("templates/user-data.yaml.tftpl", {
+      ssh_user       = var.ssh_user,
+      ssh_public_key = var.ssh_public_key
+    })
+  }
 
-#   boot_disk {
-#     initialize_params {
-#       image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
-#       size  = var.disk_size
-#       type  = "pd-standard"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
+      size  = var.disk_size
+      type  = "pd-standard"
 
-#       labels = {
-#         my_label = "ubuntu-2404-lts-amd64"
-#       }
-#     }
-#   }
+      labels = {
+        my_label = "ubuntu-2404-lts-amd64"
+      }
+    }
+  }
 
-#   network_interface {
-#     network = google_compute_network.vpc_network.name
-#     access_config {
-#     }
-#   }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 
-#   service_account {
-#     email  = google_service_account.vm_sa.email
-#     scopes = ["cloud-platform"]
-#   }
-# }
+  service_account {
+    email  = google_service_account.vm_sa.email
+    scopes = ["cloud-platform"]
+  }
+}
--- infra/automation/terraform/01_infra/network.tf
+++ infra/automation/terraform/01_infra/network.tf
@@ -3,35 +3,35 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
 }
 
-# resource "google_compute_firewall" "default" {
-#   name    = "http-access"
-#   network = google_compute_network.vpc_network.name
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80"]
-#   }
-#   target_tags   = ["http-server"]
-#   source_ranges = ["0.0.0.0/0"]
-# }
+resource "google_compute_firewall" "default" {
+  name    = "http-access"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  target_tags   = ["http-server"]
+  source_ranges = ["0.0.0.0/0"]
+}
 
-# resource "google_compute_firewall" "https" {
-#   name    = "https-access"
-#   network = google_compute_network.vpc_network.name
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["443"]
-#   }
-#   target_tags   = ["https-server"]
-#   source_ranges = ["0.0.0.0/0"]
-# }
+resource "google_compute_firewall" "https" {
+  name    = "https-access"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  target_tags   = ["https-server"]
+  source_ranges = ["0.0.0.0/0"]
+}
 
-# resource "google_compute_firewall" "ssh" {
-#   name    = "ssh-access"
-#   network = google_compute_network.vpc_network.name
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22"]
-#   }
-#   target_tags   = ["allow-ssh"]
-#   source_ranges = ["0.0.0.0/0"]
-# }
+resource "google_compute_firewall" "ssh" {
+  name    = "ssh-access"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  target_tags   = ["allow-ssh"]
+  source_ranges = ["0.0.0.0/0"]
+}
